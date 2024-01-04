2cf68162546d3b24187e262038f1f3d201fdccc9
vee1123 <79357969+vee1123@users.noreply.github.com>
2024-01-04 08:46:08 +0200

    brand change

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -53,14 +53,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://televoip.co.za'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Televoip.co.za</span>
       </a>
     </span>
   );
